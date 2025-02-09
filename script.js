a7530d8f17928629da90e57ab1102d699d6c1b4d
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-02-09 16:35:38 +0300

    load

--- script.js
+++ script.js
@@ -9,112 +9,117 @@ let taskCount = 0;
 const displayCount = (taskCount) => {
     countValue.innerText = taskCount;
 };
+
 //add a task
 const addTask = () => {
-    //the error message will be hidden when the user adds a task
+    //it takes the text typed into input field and removes the leading/trailing spaces
     const taskName = newTaskInput.value.trim();
+    //it hides the error message before each attempt to add a task
     error.style.display = "none";
-    //if there is no task, error message will be shown 
-    // (or if the user types a space)
+
+    //if the input is empty, it displays an error message after
+    // 200 ms and exits the function (return;)
     if (!taskName) {
-        //the error message will be shown after 200 milliesecond
         setTimeout(() => {
-            error.style.display = "block";//this code makes the error message visible
+            error.style.display = "block";
         }, 200);
-        //show error message if task name is invalid or empty
         return;
     }
 
-    //if taskName is not empty:
-    //the HTML structure for the new task is created
+    // Create the new task HTML
     const task = `<div class="task">
-    <input type="checkbox" class="task-check">
-    <span class="taskname">${taskName}</span>
-    <button class="edit">
-    <i class="fa-solid fa-pen-to-square"></i>
-    </button>
-    <button class="delete">
-    <i class="fa-solid fa-trash"></i>
-    </button>
-     </div>`;
+        <input type="checkbox" class="task-check">
+        <span class="taskname">${taskName}</span>
+        <button class="edit">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button class="delete">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </div>`;
 
-     //add the string (task variable) to the end of
+    //add the string (task variable) to the end of
      //taskContainer (#tasks)
-     tasksContainer.insertAdjacentHTML("beforeend",task);
+    tasksContainer.insertAdjacentHTML("beforeend", task);
 
-     const deleteButtons = document.querySelectorAll(".delete");
-     deleteButtons.forEach((button) => {
-        button.onclick = () => {
-            button.parentNode.remove();
-            taskCount -= 1;
-            displayCount(taskCount);
-        };
-     });
+    const taskDiv = tasksContainer.lastElementChild; // Get the last task div / selects the newly added task
+    const checkBox = taskDiv.querySelector(".task-check");
+    const deleteButton = taskDiv.querySelector(".delete");
+    const editButton = taskDiv.querySelector(".edit");
+    const taskNameSpan = taskDiv.querySelector(".taskname");
 
-     //select all buttons with class "edit"
-     const editButtons = document.querySelectorAll(".edit");
-     editButtons.forEach((editBtn) => {
-        //add click event to each edit button
-        editBtn.onclick = (e) => {
-            let targetElement = e.target; //gets the clicked element
-            //if the icon is clicked(a nested element),we target the parent button
-            if (!(e.target.className == "edit")) {
-                targetElement = e.target.parentElement; //get back to edit button
-            }
-            //we get the task text and put it in the span field
-            newTaskInput.value = targetElement.previousElementSibling?.innerText;
-            //remove the ed'ted task from the DOM (the 'task' div that is the parent of the button)
-            targetElement.parentNode.remove();
-            //reduce the number of tasks by one
+    // add click action to delete button
+    deleteButton.onclick = () => {
+        //if the task is not completed, reduces the taskcount
+        if (!checkBox.checked){
             taskCount -= 1;
-            //we show the updated task count on the screen
-            displayCount(taskCount);
-        };
-     });
-     //choose all checkboks elements
-     //this is important to check whether the tasks are completed or not
-     const tasksCheck = document.querySelectorAll(".task-check");
-     
-     //performs an action for each checkbox
-     tasksCheck.forEach((checkBox) => {
-        //defines an 'onchange' event for each checkbox
-        checkBox.onchange = () => {
-            //reaches the sibling element next to the checkbox(<-nextElementSibling)
-            //(classList.toggle("completed")->)adds or removes the "completed" class to the sibling element
-            //the completed class is usually  used to draw a line on the task /in CSS
-            checkBox.nextElementSibling.classList.toggle("completed");
-            //'f the checkbox is checked, one actions hppens/ if it is not checked,another action happens
+        }
+        //then removes the task from the page
+        taskDiv.remove();
+        //displays the taskCount
+        displayCount(taskCount);
+    };
+
+    //the e parameter contains the information of the click event
+    editButton.onclick = (e) => {
+        //saves the c-exact element (target) that the  user clicked
+        //in the targetElement variable
+        //if the button was clicked directly, the targetElemment becomes the direct editButton
+        let targetElement = e.target;
+        //if the user clicks on the icon (<i>) inside the button,
+        //e.target becomes an icon instead of a direct button(edit button)
+        //in this situation, set the icon's parentElement (yani the actual button)
+        //as targetElement
+        if (!(e.target.className === "edit")) {
+            targetElement = e.target.parentElement;
+        }//                 |  .   .   .   .   .   .   .   .   . | -> selects the previous sibiling element of the button (<span class="taskname">)
+        newTaskInput.value = targetElement.previousElementSibling.innerText; //takes the name of the task with innerText 
+        //reaches the button's parent <div class="task"> element.
+        //removes this item completely with .remove
+        targetElement.parentNode.remove();
+        taskCount -= 1;
+        displayCount(taskCount);
+    };
+
+    // Handle/process checkbox change (when checkbox is clicked)
+    checkBox.onchange = () => {
+        //strikes out the text by adding the "completed" class
+        checkBox.nextElementSibling.classList.toggle("completed");
+        //if checked/marked, taskCount is reduces/decreases by one because the task is completed
+        if (checkBox.checked) {
+            taskCount -= 1;
+        } else {     //if unchecked taskCount is increases by one because the task is becomes active again
+            taskCount += 1;
+        }
+        displayCount(taskCount);
+    };
+
+    // Make the task div clickable to toggle/change checkbox (even if the checkbox itself is not clicked)
+    taskDiv.addEventListener("click", function (e) {
+        // Only toggle(change the checkbox status ) if the target is not the edit or delete button
+        if (e.target !== editButton && e.target !== deleteButton && e.target !== checkBox) {
+            checkBox.checked = !checkBox.checked;
+            taskDiv.classList.toggle("completed");
+
             if (checkBox.checked) {
-                //if checked, taskCount is reduced by one because the task is completed
                 taskCount -= 1;
-            } //if not checked, taskCount is increased by one because the task is not completed
-            else {
+            } else {
                 taskCount += 1;
             }
-            //after the updating taskCount 
             displayCount(taskCount);
-        };
-     });
-     //when a new task is added, the number of tasks increases by one
-     taskCount += 1;
-     //calls a function to display the task count
-     displayCount(taskCount);
-     //this code clears the input field after adding the task is finished
-     newTaskInput.value = "";
-};
+        }
+    });
 
-//it couses the function 'addTask' to run when the button is clicked
-addBtn.addEventListener("click", addTask);
-
-//runs a function when the page is loads
-window.onload = () => {
-    //resets the task count
-    taskCount = 0;
-    //shows the task count(as zero)
+    taskCount += 1;
     displayCount(taskCount);
-    //clears the input field
     newTaskInput.value = "";
 };
 
-//clicking the add button starts the process fo adding a new task
+//run addTask function when user clicks "add" button
 addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+};
